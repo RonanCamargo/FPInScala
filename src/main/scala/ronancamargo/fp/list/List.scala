005b43fbf1269d279186c258fa46da10f7921378
RonanCamargo <ronan.cam@gmail.com>
2020-06-28 00:32:39 -0300

    Add List functions

--- src/main/scala/ronancamargo/fp/list/List.scala
+++ src/main/scala/ronancamargo/fp/list/List.scala
@@ -7,8 +7,56 @@ case class NonEmpty[A] (head: A, tail: List[A]) extends List[A]
 
 object List {
 
-  def apply[A] (as: A*): List[A] = {
+  def apply[A](as: A*): List[A] = {
     if(as.isEmpty) Nil
     else NonEmpty(as.head, apply(as.tail: _*))
   }
+
+  def fill[A](elem: A, n: Int): List[A] = {
+    n match {
+      case 0 => Nil
+      case 1 => NonEmpty(elem, Nil)
+      case _ => NonEmpty(elem, fill(elem, n - 1))
+    }
+  }
+
+  def tail[A](list: List[A]): List[A] = {
+    list match {
+      case NonEmpty(_, tail) => tail
+      case _ => list
+    }
+  }
+
+  @scala.annotation.tailrec
+  def drop[A](n: Int, list: List[A]): List[A] = {
+    n match {
+      case x if x > 1 => drop(x-1, tail(list))
+      case 1 => tail(list)
+      case 0 => Nil
+    }
+  }
+
+  @scala.annotation.tailrec
+  def uncurriedDropWhile[A](list: List[A], f: A => Boolean): List[A] = {
+    list match {
+      case NonEmpty(h, t) if f(h) => uncurriedDropWhile(t, f)
+      case _ => list
+    }
+  }
+
+  @scala.annotation.tailrec
+  def dropWhile[A](list: List[A])(f: A => Boolean): List[A] = {
+    list match {
+      case NonEmpty(h, t) if f(h) => dropWhile(t)(f)
+      case _ => list
+    }
+  }
+
+  def append[A](l1: List[A], l2: List[A]): List[A] = {
+    l1 match {
+      case Nil => l2
+      case NonEmpty(h, t) => NonEmpty(h, append(t, l2))
+    }
+  }
+
 }
--- src/test/scala/ronancamargo/fp/list/ListSpec.scala
+++ src/test/scala/ronancamargo/fp/list/ListSpec.scala
@@ -2,6 +2,7 @@ package ronancamargo.fp.list
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import ronancamargo.fp.list.List._
 
 class ListSpec extends AnyFlatSpec
   with Matchers{
@@ -14,6 +15,32 @@ class ListSpec extends AnyFlatSpec
 
   it should "be NonEmpty" in {
     List(1,2,3) should be(NonEmpty(1, NonEmpty(2, NonEmpty(3, Nil))))
+  }
+
+  it should "fill with n elements" in {
+    val filled = fill("a", 3)
+    filled should be(List("a","a","a"))
+  }
+
+  it should "drop the first n elements" in {
+    val dropped = drop(3, fill('A', 5))
+    dropped should be(List('A','A'))
+  }
+
+  it should "drop while f result is true, uncurried" in {
+    val dropped = uncurriedDropWhile(List(1,3,5,10,11), (x: Int) => x < 10)
+    dropped should be(List(10,11))
+  }
+
+  it should "drop while f result is true" in {
+    val dropped = dropWhile(List(1,3,5,10,11))(x => x < 10)
+    dropped should be(List(10,11))
+  }
+
+
+  it should "append two lists" in {
+    val appended = append(List(1,2), List(3,4,5))
+    appended should be(List(1,2,3,4,5))
   }
 
 }
