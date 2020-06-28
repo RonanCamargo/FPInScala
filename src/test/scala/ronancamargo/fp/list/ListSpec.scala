180622dce236654f26fb67dd377fa30cd92c42ea
RonanCamargo <ronan.cam@gmail.com>
2020-06-27 23:38:42 -0300

    Add basic unit tests

--- src/test/scala/ronancamargo/fp/list/ListSpec.scala
+++ src/test/scala/ronancamargo/fp/list/ListSpec.scala
@@ -6,4 +6,14 @@ import org.scalatest.matchers.should.Matchers
 class ListSpec extends AnyFlatSpec
   with Matchers{
 
+  behavior of "List"
+
+  it should "be Nil" in {
+    List() should be(Nil)
+  }
+
+  it should "be NonEmpty" in {
+    List(1,2,3) should be(NonEmpty(1, NonEmpty(2, NonEmpty(3, Nil))))
+  }
+
 }
