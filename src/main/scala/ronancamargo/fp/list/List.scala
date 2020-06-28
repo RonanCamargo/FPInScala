c5439efb3c07533b023607db6f2814ded236ec64
RonanCamargo <ronan.cam@gmail.com>
2020-06-27 23:35:03 -0300

    Add subtypes and companion object

--- src/main/scala/ronancamargo/fp/list/List.scala
+++ src/main/scala/ronancamargo/fp/list/List.scala
@@ -1,5 +1,14 @@
 package ronancamargo.fp.list
 
-sealed trait List[A] {
+sealed trait List[+A]
 
+case object Nil extends List[Nothing]
+case class NonEmpty[A] (head: A, tail: List[A]) extends List[A]
+
+object List {
+
+  def apply[A] (as: A*): List[A] = {
+    if(as.isEmpty) Nil
+    else NonEmpty(as.head, apply(as.tail: _*))
+  }
 }
